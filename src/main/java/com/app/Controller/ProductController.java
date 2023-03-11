546647660fe734867b3c15ed18e467934277b953
Pranjali Kolte <kolte.pranjali@gmail.com>
2023-03-11 15:25:50 +0530

    getapi

--- src/main/java/com/app/Controller/ProductController.java
+++ src/main/java/com/app/Controller/ProductController.java
@@ -1,10 +1,12 @@
 package com.app.Controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +33,13 @@ public class ProductController {
 	             return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
 	         }
 	     }
+	     
+	     
+	     @GetMapping("/products")
+	     public ResponseEntity<List<Products>> getAllProducts() {
+	         List<Products> products = productRepository.findAll();
+	         return new ResponseEntity<>(products, HttpStatus.OK);
+	     }
+
 	 }
 
